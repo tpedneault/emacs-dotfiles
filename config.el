913d5d9076c0b63a19740edcc5e26eec082db2e3
Thomas Pedneault <thomas@pedneault.dev>
2021-10-10 20:37:20 -0400

    Finished implementing programming configuration

--- config.el
+++ config.el
@@ -14,7 +14,7 @@
 
 (toggle-scroll-bar -1)
 
-(setq visible-bell 1)
+(setq visible-bell nil)
 
 (show-paren-mode 1)
 
@@ -29,13 +29,33 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-(load-theme 'zenburn t)
-
-(set-frame-font "Hack 11" nil t)
-
-(use-package doom-modeline
+(use-package exec-path-from-shell
   :ensure t
-  :init (doom-modeline-mode 1))
+  :config (exec-path-from-shell-initialize))
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :config
+  (load-theme 'sanityinc-tomorrow-night t))
+
+(set-frame-font "Hack Nerd Font 12" nil t)
+
+(use-package telephone-line
+  :ensure t
+  :init 
+  (setq telephone-line-lhs
+	'((evil   . (telephone-line-evil-tag-segment))
+	  (accent . (telephone-line-vc-segment
+		     telephone-line-erc-modified-channels-segment
+		     telephone-line-process-segment))
+	  (nil    . (telephone-line-minor-mode-segment
+		     telephone-line-buffer-segment))))
+  (setq telephone-line-rhs
+	'((nil    . (telephone-line-misc-info-segment))
+	  (accent . (telephone-line-major-mode-segment))
+	  (evil   . (telephone-line-airline-position-segment))))
+
+  :config (telephone-line-mode))
 
 (use-package org-bullets
   :ensure t
@@ -44,6 +64,7 @@
 (use-package dashboard
   :ensure t
   :config (dashboard-setup-startup-hook)
+  (setq dashboard-startup-banner "~/.emacs.d/glasses-with-mustache.png")
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t))
 
@@ -80,19 +101,20 @@
 
 (use-package ivy-rich
   :ensure t
-  :after ivy
-  :init (ivy-rich-mode 1)
-  :custom
-  (ivy-virtual-abbreviate 'full
-			  ivy-rich-switch-buffer-align-virtual-buffer t
-			  ivy-rich-path-style 'abbrev)
-  :config (ivy-set-display-transformer 'ivy-switch-buffer
-				       'ivy-rich-switch-buffer-transformer))
+  :init (ivy-rich-mode 1))
+
+(use-package all-the-icons-ivy-rich
+  :ensure t
+  :init (all-the-icons-ivy-rich-mode 1))
 
 (use-package swiper
   :after ivy
   :bind (("C-s" . swiper)
 	 ("C-r" . swiper)))
+
+(setq ivy-virtual-abbreviate 'abbreviate
+      ivy-rich-path-style 'abbrev
+      all-the-icons-ivy-rich-icon-size 1.1)
 
 (use-package treemacs
   :ensure t
@@ -183,20 +205,51 @@
   :ensure t
   :config (which-key-mode))
 
+(use-package smartparens
+  :ensure t
+  :config (smartparens-global-mode))
+
+(use-package magit
+  :ensure t)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      company-minimum-prefix-length 1
+      lsp-lens-enable t
+      lsp-signature-auto-activate nil)
+
 (use-package lsp-mode
   :ensure t
-  :init (setq lsp-keymap-prefix "C-c l")
+  :init (setq lsp-keymap-prefix "C-l")
   :hook (lsp-mode . lsp-enable-which-key-integration)
   :commands (lsp lsp-deferred))
 
 (use-package lsp-ui
   :ensure t
   :after lsp
-  :commands lsp-ui-mode)
+  :commands lsp-ui-mode
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  )
+
+(setq lsp-ui-doc-enable t
+      lsp-ui-doc-use-childframe t
+      lsp-ui-doc-position 'top
+      lsp-ui-doc-include-signature t
+      lsp-ui-sideline-enable nil
+      lsp-ui-flycheck-enable t
+      lsp-ui-flycheck-list-position 'right
+      lsp-ui-flycheck-live-reporting t
+      lsp-ui-peek-enable t
+      lsp-ui-peek-list-width 60
+      lsp-ui-peek-peek-height 25
+      )
 
 (use-package lsp-ivy
   :ensure t
-  :commands lsp-ivy-workspace-symbol)
+  :commands lsp-ivy-workspace-symbol
+  :config (global-set-key ))
 
 (use-package lsp-treemacs 
   :ensure t 
@@ -205,3 +258,36 @@
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
+
+(use-package company
+  :ensure t
+  :config 
+  (setq company-idle-delay 0)
+  (global-company-mode 1)
+  )
+
+(use-package dap-mode
+  :ensure t
+  :config (setq dap-auto-configure-features '(sessions locals controls tooltip)))
+
+(use-package dart-mode
+  :custom 
+  (dart-sdk-path (concat (getenv "HOME") "/.flutter/bin/cache/dart-sdk/")
+		 dart-format-on-save t))
+
+(use-package lsp-dart
+  :ensure t)
+
+(add-hook 'dart-mode-hook 'lsp-deferred)
+
+(add-hook 'c-mode-hook 'lsp-deferred)
+(add-hook 'c++-mode-hook 'lsp-deferred)
+
+(use-package php-mode
+  :ensure t
+  :mode ("\\.php\\'" . php-mode))
+
+(use-package phpunit
+  :ensure t)
+
+(add-hook 'php-mode-hook 'lsp-deferred)
--- init.el
+++ init.el
@@ -22,9 +22,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(all-the-icons-ivy-rich-mode t)
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#c5c8c6"))
+ '(beacon-color "#cc6666")
  '(custom-safe-themes
-   '("e76668f7efa2c9297c3998aba61876e757c99c1e5325c135433cf4f53a95df15" "d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "c1284dd4c650d6d74cfaf0106b8ae42270cab6c58f78efc5b7c825b6a4580417" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "d36e851fab767ad68cdabbae5784dbe88d090b011dd721eee8e527e21f5422af" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" default))
+   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "e76668f7efa2c9297c3998aba61876e757c99c1e5325c135433cf4f53a95df15" "d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "c1284dd4c650d6d74cfaf0106b8ae42270cab6c58f78efc5b7c825b6a4580417" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "d36e851fab767ad68cdabbae5784dbe88d090b011dd721eee8e527e21f5422af" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" default))
  '(electric-indent-mode nil)
+ '(fci-rule-color "#373b41")
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode 'dark)
  '(gdb-many-windows t)
  '(gdb-show-main t)
  '(helm-completion-style 'emacs)
@@ -36,7 +44,29 @@
 		  :weight ultra-bold :height 0.99))))
  '(markdown-code-face ((t (:inherit consolas))))
  '(package-selected-packages
-   '(lsp-treemacs lsp-ivy monokai-theme obsidian-theme zenburn-theme lsp-ui lsp-mode web-mode treemacs-all-the-icons page-break-lines doom-modeline all-the-icons-ivy-rich ivy-rich counsel all-the-icons-ivy evil-collection dashboard cmake-ide irony-mode rtags-helm company-rtags js2-mode vue-mode smartparens company-mode go-mode-load go-imports go-mode dracula-theme treemacs-icons-dired treemacs-projectile treemacs-evil projectile org-sticky-header gruvbox-theme badger-theme rustic which-key dap-mode helm-lsp robe all-the-icons neotree yasnippet-snippets flycheck irony-eldoc company-irony minions mood-line atom-one-dark-theme evil helm use-package)))
+   '(exec-path-from-shell php-mode hover lsp-dart magit telephone-line spaceline-all-the-icons spaceline smartparens color-theme-sanityinc-tomorrow company lsp-treemacs lsp-ivy monokai-theme obsidian-theme zenburn-theme lsp-ui lsp-mode web-mode treemacs-all-the-icons page-break-lines all-the-icons-ivy-rich ivy-rich counsel all-the-icons-ivy evil-collection dashboard cmake-ide irony-mode rtags-helm company-rtags js2-mode vue-mode company-mode go-mode-load go-imports go-mode dracula-theme treemacs-icons-dired treemacs-projectile treemacs-evil projectile org-sticky-header gruvbox-theme badger-theme rustic which-key dap-mode helm-lsp robe all-the-icons neotree yasnippet-snippets flycheck irony-eldoc company-irony minions mood-line atom-one-dark-theme evil helm use-package))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68")))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
